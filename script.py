6b9d9fd16cb31f75bcd48beaf87370b0b92a2ef2
Rani Haddad <rani.haddad@outlook.com>
2025-02-27 19:30:56 +0300

    add help handler

--- script.py
+++ script.py
@@ -4,14 +4,26 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 TOKEN = ''
 
 
+async def help(update, context):
+    print('help request recieved')
+    await update.message.reply_text("what can i help you with?")
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print('start request recieved')
     await update.message.reply_text("Hello! Welcome to our E-shop.")
 
 
+def load_commands(application):
+    print('adding handlers...')
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler('help', help))
+
+
 def main():
     application = Application.builder().token(TOKEN).concurrent_updates(True).build()
-    application.add_handler(CommandHandler("start", start))
+    load_commands(application)
+    print("Telegram Bot started!", flush=True)
     application.run_polling()
 
 
