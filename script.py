114e0ca1c08b058312f6bb5882cdc5fcd262431c
Rani Haddad <rani.haddad@outlook.com>
2025-02-27 19:30:03 +0300

    add start handler

--- script.py
+++ script.py
@@ -1,10 +1,17 @@
-from telegram.ext import Application
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = ''
 
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print('start request recieved')
+    await update.message.reply_text("Hello! Welcome to our E-shop.")
+
+
 def main():
     application = Application.builder().token(TOKEN).concurrent_updates(True).build()
+    application.add_handler(CommandHandler("start", start))
     application.run_polling()
 
 
